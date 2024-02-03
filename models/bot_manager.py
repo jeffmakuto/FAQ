4183c0066e46c1171779b4ae86f25db61f4066fb
jeffmakuto <hjeffmakuto@gmail.com>
2024-02-03 19:56:55 +0300

    Rule based class

---
+++ models/bot_manager.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module that handles the bot's logic features """
+
+
+class RuleBasedBot:
+    """
+    Takes input processes it and answers back. if it doesn't understand,
+    it answers 'Sorry i don't understand. please ask me anything else'
+    """
