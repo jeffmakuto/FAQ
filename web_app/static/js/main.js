6ee9884c8e6ac031287c9b8be763efd139ab2a89
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-02-03 11:34:09 +0300

    Update main.js

--- web_app/static/js/main.js
+++ web_app/static/js/main.js
@@ -1,64 +1,18 @@
-/* static/script.js */
-document.addEventListener('DOMContentLoaded', function () {
-    /* Function to send initial greeting */
-    sendInitialGreeting();
-    
-    /* Enable sending message on Enter key press */
-    document.getElementById('user-input').addEventListener('keyup', function (event) {
-        if (event.key === 'Enter') {
-            sendMessage();
-        }
-    });
-});
-
-function sendMessage() {
-    const userInput = document.getElementById('user-input').value;
-    const chatContainer = document.getElementById('chat-container-content');
-
-    /* Display user message */
-    chatContainer.innerHTML += `<div class="message">User: ${userInput}</div>`;
-
-    /* Send user input to Flask backend */
-    fetch('/api/bot', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ user_input: userInput }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        /* Display bot response */
-        chatContainer.innerHTML += `<div class="message">Bot: ${data.bot_response}</div>`;
-        
-        /* Scroll to the bottom of the chat container */
-        chatContainer.scrollTop = chatContainer.scrollHeight;
-    })
-    .catch(error => console.error('Error:', error));
-
-    /* Clear user input */
-    document.getElementById('user-input').value = '';
-}
-
-/* Function to toggle the enlarged state */
 function toggleEnlarge() {
     const chatContainer = document.getElementById('chat-container');
-    chatContainer.classList.toggle('enlarged');
-}
+    const enlargedClass = 'enlarged';
 
-/* Function to send initial greeting */
-function sendInitialGreeting() {
-    const chatContainer = document.getElementById('chat-container-content');
-    
-    /* Display initial greeting */
-    const greetingMessage = document.createElement('div');
-    greetingMessage.classList.add('message');
-    greetingMessage.innerHTML = 'Hi there! I\'m Bota! How can I help?';
-    
-    chatContainer.appendChild(greetingMessage);
+    chatContainer.classList.toggle(enlargedClass);
 
-    /* Remove the greeting message after 5 seconds */
-    setTimeout(() => {
-        greetingMessage.remove();
-    }, 3000);
+    if (chatContainer.classList.contains(enlargedClass)) {
+        chatContainer.style.left = '50%';
+        chatContainer.style.transform = 'translateX(-50%)';
+        chatContainer.style.width = '300px';
+        chatContainer.style.height = '300px';
+    } else {
+        chatContainer.style.left = 'auto';
+        chatContainer.style.transform = 'none';
+        chatContainer.style.width = 'auto';
+        chatContainer.style.height = 'auto';
+    }
 }
