9aa735110eb5e1270fdc2eff8a6b141f69536db3
jeffmakuto <hjeffmakuto@gmail.com>
2024-02-03 08:14:30 +0000

    Updates

--- web_app/static/js/enlargeFunctions.js
+++
@@ -1,6 +0,0 @@
-/* enlargeFunctions.js */
-export function toggleEnlarge() {
-    const chatContainer = document.getElementById('chat-container');
-    chatContainer.classList.toggle('enlarged');
-}
-
