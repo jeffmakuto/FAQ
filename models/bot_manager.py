555230315b69d19878cfddb9d6697d7b84871c8b
jeffmakuto <hjeffmakuto@gmail.com>
2024-02-03 22:30:19 +0300

    Add tests and minimal code to pass them

--- models/bot_manager.py
+++ models/bot_manager.py
@@ -5,5 +5,24 @@
 class RuleBasedBot:
     """
     Takes input processes it and answers back. if it doesn't understand,
-    it answers 'Sorry i don't understand. please ask me anything else'
+    it answers 'I don't have an answer for that, sorry'
     """
+    def __init__(self):
+        """ Initialize the database to store the data """
+        self.db = {}
+
+    def add_to_db(self, q, a):
+        """ Adds the new query to the database """
+        self.db[q] = a
+
+    def respond(self, user_input):
+        """
+        Checks if an aswer is provided in the db and responds
+        If the response isn't found it writes a custom message
+        to inform the user response isn't available
+        """
+        if user_input in self.db:
+            return self.db[user_input]
+        else:
+            self.add_to_db(user_input, "...")
+            return "I don't have answer for that, sorry"
---
+++ tests/test_models/test_bot_manager.py
@@ -0,0 +1,15 @@
+""" Test for the bot manager.py file """
+import unittest
+from models.bot_manager import RuleBasedBot
+
+class TestRuleBasedBot(unittest.Testcase):
+    """ Test for the functionality of the RuleBasedBot class """
+    def test_known_query(self):
+        """
+        Instance of RuleBasedBot with a known question
+        in the databse
+        """
+        bot = RuleBasedBot()
+        bot.add_to_db("What is the KQ acronym in full?", "Kenya Airways")
+        response = bot.respond("What is the KQ acronym in ful?")
+        self.assertEqual(response, "Kenya Airways")
