b638e127cb92e40d95247d8aa5f6c092ce4531a4
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-02-09 19:03:10 +0300

    Update index.py with smtp configurations import

--- back_end/blueprints/index.py
+++ back_end/blueprints/index.py
@@ -3,7 +3,7 @@
 from . import app_blueprints
 from flask import jsonify, render_template, request
 from models.bot_manager import RuleBasedBot, Admin
-from config import *
+from config import smtp_config_instance
 
 bot_instance = RuleBasedBot()
 admin_instance = Admin(bot_instance)
@@ -18,12 +18,19 @@ def home():
 @app_blueprints.route('/bot', methods=['POST'])
 def bot():
     """
-    
     Handles incoming user input, processes it with FAQ bot logic,
     and returns a JSON response with the bot's reply.
-
     """
     user_input = request.json.get('user_input')
-    bot_response = bot_instance.respond(user_input, admin_instance,
-            smtp_server, smtp_port, sender_email, sender_password, recipient_email)
+
+    # Use SMTP configuration from smtp_config_instance
+    bot_response = bot_instance.respond(
+        user_input, admin_instance,
+        smtp_config_instance.smtp_server,
+        smtp_config_instance.smtp_port,
+        smtp_config_instance.sender_email,
+        smtp_config_instance.sender_password,
+        smtp_config_instance.recipient_email
+    )
+
     return jsonify({'bot_response': bot_response})
