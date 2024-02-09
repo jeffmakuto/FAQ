a23e552b434e860a23a986804ac45741e40806da
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-02-09 19:07:37 +0300

    Update __init__.py

--- back_end/blueprints/__init__.py
+++ back_end/blueprints/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-app_blueprints = Blueprint('app_blueprints', __name__, url_prefix='/api/v1')
+app_blueprints = Blueprint('app_blueprints', __name__)
 
 from .index import *
