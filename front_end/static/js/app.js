71833a5b6ae4a765d8d7a0576c37f5e485eec23b
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-02-09 19:24:49 +0300

    Update app.js

--- front_end/static/js/app.js
+++ front_end/static/js/app.js
@@ -1,3 +1,6 @@
+/* Import Axios */
+import axios from 'axios';
+
 const app = Vue.createApp({
     data() {
         return {
@@ -14,20 +17,42 @@ const app = Vue.createApp({
         },
 
         sendMessage() {
-            const userMessage = { text: "You: " + this.message, isUser: true };
-            const botReply = { text: "Bota: " + this.bota, isUser: false };
+            /* Check if the user input is "Hi" */
+            if (this.message.trim().toLowerCase() === 'hi') {
+                const userMessage = { text: "You: " + this.message, isUser: true };
+                const botReply = { text: "Bota: Hello! How may I be of help today?", isUser: false };
 
-            this.messages.push(userMessage);
-            this.messages.push(botReply);
+                this.messages.push(userMessage);
+                this.messages.push(botReply);
 
-            /* Scroll to the bottom after adding a new message */
-            this.$nextTick(() => {
-                const container = this.$refs.messageContainer;
-                container.scrollTop = container.scrollHeight;
-            });
+                /* Scroll to the bottom after adding a new message */
+                this.$nextTick(() => {
+                    const container = this.$refs.messageContainer;
+                    container.scrollTop = container.scrollHeight;
+                });
 
-            /* Clear the input field */
-            this.message = "";
+                /* Clear the input field */
+                this.message = "";
+            } else {
+                /* Send user input to the backend */
+                axios.post('/bot', { user_input: this.message })
+                    .then(response => {
+                        const botReply = { text: `Bota: ${response.data.bot_response}`, isUser: false };
+                        this.messages.push(botReply);
+
+                        /* Scroll to the bottom after adding a new message */
+                        this.$nextTick(() => {
+                            const container = this.$refs.messageContainer;
+                            container.scrollTop = container.scrollHeight;
+                        });
+                    })
+                    .catch(error => {
+                        console.error('Error communicating with the backend:', error);
+                    });
+
+                /* Clear the input field */
+                this.message = "";
+            }
         },
 
         clearChat() {
