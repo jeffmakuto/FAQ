608d44416481bb458b47c184a00e324b07d80068
jeffmakuto <hjeffmakuto@gmail.com>
2024-02-10 16:51:28 +0300

    Update vue.config.js

--- front_end/bot_vue/vue.config.js
+++ front_end/bot_vue/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+	transpileDependencies: true,
+	chainWebpack: (config) => {
+		config.plugin('define').tap((args) => {
+			const defineArgs = args[0];
+
+			/* Add or modify the feature flags here */
+			defineArgs['__VUE_OPTIONS_API__'] = true;
+			defineArgs['__VUE_PROD_DEVTOOLS__'] = false;
+			defineArgs['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = false;
+			return args;
+		});
+	},
+});
+
