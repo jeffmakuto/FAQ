781fe067168ed51d3318a8178dd672ef497d8640
jeffmakuto <hjeffmakuto@gmail.com>
2024-02-10 17:52:20 +0300

    Update app.py

--- back_end/app.py
+++ back_end/app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 from blueprints import app_blueprints
 import os
 
 app = Flask(__name__)
 
+app.config.from_object(__name__)
+
+CORS(app, resources={r"/*":{'origins':}})
+
 # Global strict slashes
 app.url_map.strict_slashes = False
 
 # Configure templates and static folders for the web application
-app.template_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'front_end', 'templates'))
-app.static_folder= os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'front_end', 'static'))
+app.template_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'front_end', 'bot_vue', 'public'))
+app.static_folder= os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'front_end', 'bot_vue', 'src'))
 
 # Register blueprint
 app.register_blueprint(app_blueprints)
