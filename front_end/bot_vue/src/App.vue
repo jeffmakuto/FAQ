2e8ca27a51cdcac3fe4dafac56ebf144a84980fd
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-02-10 12:22:11 +0300

    Update App.vue

--- front_end/bot_vue/src/App.vue
+++ front_end/bot_vue/src/App.vue
@@ -7,9 +7,64 @@
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+        return {
+            isChatBoxEnlarged: false,
+            message: "",
+            bota: "Hello! my name is Bota! How may I be of help today?",
+            messages: [] /* Array to store messages */
+        };
+    },
+
+    methods: {
+        toggleChatBoxSize() {
+            this.isChatBoxEnlarged = !this.isChatBoxEnlarged;
+        },
+
+        sendMessage() {
+            /* Check if the user input is "Hi" */
+            if (this.message.trim().toLowerCase() === 'hi') {
+                const userMessage = { text: "You: " + this.message, isUser: true };
+                const botReply = { text: "Bota: Hello! How may I be of help today?", isUser: false };
+
+                this.messages.push(userMessage);
+                this.messages.push(botReply);
+
+                /* Scroll to the bottom after adding a new message */
+                this.$nextTick(() => {
+                    const container = this.$refs.messageContainer;
+                    container.scrollTop = container.scrollHeight;
+                });
+
+                /* Clear the input field */
+                this.message = "";
+            } else {
+                /* Send user input to the backend */
+                axios.post('/bot', { user_input: this.message })
+                    .then(response => {
+                        const botReply = { text: `Bota: ${response.data.bot_response}`, isUser: false };
+                        this.messages.push(botReply);
+
+                        /* Scroll to the bottom after adding a new message */
+                        this.$nextTick(() => {
+                            const container = this.$refs.messageContainer;
+                            container.scrollTop = container.scrollHeight;
+                        });
+                    })
+                    .catch(error => {
+                        console.error('Error communicating with the backend:', error);
+                    });
+
+                /* Clear the input field */
+                this.message = "";
+            }
+        },
+
+        clearChat() {
+            /* Clear all messages */
+            this.messages = [];
+        }
+    }
 }
 </script>
 
