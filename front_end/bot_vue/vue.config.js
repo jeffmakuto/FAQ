b418f956b03e55cf12250e42b978e4b471ba33a7
jeffmakuto <hjeffmakuto@gmail.com>
2024-02-11 13:47:55 +0300

    Updates

--- front_end/bot_vue/vue.config.js
+++ front_end/bot_vue/vue.config.js
@@ -1,24 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-	devServer: {
-		host: '0.0.0.0',
-		port: 8080,
-		https: true,
-		key: path.resolve(__dirname, 'key.pem'),
-		cert: path.resolve(__dirname, 'cert.pem'),
-	},
-	transpileDependencies: true,
-	chainWebpack: (config) => {
-		config.plugin('define').tap((args) => {
-			const defineArgs = args[0];
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+  },
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin('define').tap((args) => {
+      const defineArgs = args[0];
 
-			/* Add or modify the feature flags here */
-			defineArgs['__VUE_OPTIONS_API__'] = true;
-			defineArgs['__VUE_PROD_DEVTOOLS__'] = false;
-			defineArgs['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = false;
-			return args;
-		});
-	},
+      /* Add or modify the feature flags here */
+      defineArgs['__VUE_OPTIONS_API__'] = true;
+      defineArgs['__VUE_PROD_DEVTOOLS__'] = false;
+      defineArgs['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = false;
+
+      return args;
+    });
+  },
 });
-
