8f27f37f928f6bde09f37abeb2198887e36f0682
jeffmakuto <hjeffmakuto@gmail.com>
2024-02-11 13:04:21 +0300

    Update vue.config.js to run on all hosts, i.e., 0.0.0.0

--- front_end/bot_vue/vue.config.js
+++ front_end/bot_vue/vue.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+	devServer: {
+		host: '0.0.0.0',
+		port: 8080,
+	},
 	transpileDependencies: true,
 	chainWebpack: (config) => {
 		config.plugin('define').tap((args) => {
