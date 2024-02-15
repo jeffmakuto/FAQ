d8bfe00711ad143f736ae94de089f6a1a740c30f
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-02-15 16:10:29 +0300

    Update Bot.vue

--- front_end/bot_vue/src/components/Bot.vue
+++ front_end/bot_vue/src/components/Bot.vue
@@ -49,7 +49,7 @@ export default {
 			this.messages.push(userMessage);
 
 			/* Send the user's message to the backend */
-			axios.post('http://localhost:5000/bot', { user_input: this.message })
+			axios.post('http://54.237.117.130:5000/bot', { user_input: this.message })
 				.then(response => {
 					/* Get the bot's response */
 					const botReply = { text: `Bota: ${response.data.bot_response}`, isUser: false };
