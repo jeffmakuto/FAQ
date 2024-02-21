7ae340ef929809cc48da5735497730a8e4f55bc2
root <hjeffmakuto@gmail>
2024-02-21 12:37:06 +0000

    Create and use secret key for flask socket events

--- back_end/app.py
+++ back_end/app.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """
-Bot's Flask application with SocketIO support.
+Description: Simple Flask application with SocketIO support.
 """
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from blueprints import app_blueprints
+from secret_key_gen import SecretKeyGenerator
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {'origins': '*'}})
 app.url_map.strict_slashes = False
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = SecretKeyGenerator.generate_secret_key()
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 app.register_blueprint(app_blueprints)
@@ -22,4 +23,3 @@ if __name__ == '__main__':
     Entry point for the application. Runs the Flask app with SocketIO support.
     """
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
-
