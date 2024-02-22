989332175e69e5b53189438b8d22a6823a62832b
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-02-22 09:12:31 +0300

    Update app.py

--- back_end/app.py
+++ back_end/app.py
@@ -5,15 +5,16 @@ Description: Simple Flask application with SocketIO support.
 
 from flask import Flask
 from flask_cors import CORS
-from flask_socketio import SocketIO
-from blueprints import app_blueprints
+from blueprints import app_blueprints, socketio
 from secret_key_gen import SecretKeyGenerator
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {'origins': '*'}})
 app.url_map.strict_slashes = False
 app.config['SECRET_KEY'] = SecretKeyGenerator.generate_secret_key()
-socketio = SocketIO(app, cors_allowed_origins="*")
+
+# Initialize SocketIO with the Flask app
+socketio.init_app(app)
 
 app.register_blueprint(app_blueprints)
 
