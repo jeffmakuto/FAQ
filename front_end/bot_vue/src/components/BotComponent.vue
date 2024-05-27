d63cca2c77bef172486c75aebce5aa7e1d977f2a
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-05-27 15:43:40 +0300

    Update BotComponent.vue

--- front_end/bot_vue/src/components/BotComponent.vue
+++ front_end/bot_vue/src/components/BotComponent.vue
@@ -62,7 +62,7 @@ export default {
       const userMessage = { text: this.message, isUser: true, timestamp: this.getCurrentTimestamp() };
       this.messages.push(userMessage);
 
-      axios.post('http://localhost:5000/bot', { user_input: this.message })
+      axios.post('https://v1-bot-phlc.onrender.com/bot', { user_input: this.message })
         .then(response => {
           const botReply = {
             text: `${response.data.bot_response}`,
