d54e600cc45e74c3be3ece6297150c14870ebfc1
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-11-04 15:29:35 +0300

    Update index.py

--- back_end/blueprints/index.py
+++ back_end/blueprints/index.py
@@ -14,23 +14,25 @@ def home():
     """ Renders the index.html template """
     return render_template('index.html')
 
-
 @app_blueprints.route('/bot', methods=['POST'])
 def bot():
-    """
-    Handles incoming user input, processes it with FAQ bot logic,
-    and returns a JSON response with the bot's reply.
-    """
-    user_input = request.json.get('user_input')
+    try:
+        user_input = request.json.get('user_input')
+        if user_input is None:
+            return jsonify({'error': 'User input is required'}), 400
 
-    # Use SMTP configuration from smtp_config_instance
-    bot_response = bot_instance.respond(
-        user_input, admin_instance,
-        smtp_config_instance.smtp_server,
-        smtp_config_instance.smtp_port,
-        smtp_config_instance.sender_email,
-        smtp_config_instance.sender_password,
-        smtp_config_instance.recipient_email
-    )
+        # Use SMTP configuration from smtp_config_instance
+        bot_response = bot_instance.respond(
+            user_input, admin_instance,
+            smtp_config_instance.smtp_server,
+            smtp_config_instance.smtp_port,
+            smtp_config_instance.sender_email,
+            smtp_config_instance.sender_password,
+            smtp_config_instance.recipient_email
+        )
 
-    return jsonify({'bot_response': bot_response})
+        return jsonify({'bot_response': bot_response})
+
+    except Exception as e:
+        app.logger.error(f"Error processing request: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
